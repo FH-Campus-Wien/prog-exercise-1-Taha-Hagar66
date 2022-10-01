25e329ff5efbe5dba6c3b9b0011638561d9bd3e5
taha6 <taha.hagar@stud.fh-campuswien.ac.at>
2022-10-01 23:06:37 +0200

    Task 1 & 2 Done!

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,17 +1,28 @@
 package at.ac.fhcampuswien;
 
+import sun.awt.windows.WPrinterJob;
+
 import java.util.Scanner;
 
 public class App {
 
     //todo Task 1
     public void sayHelloWorld(){
-        // input your solution here
+        System.out.println("Hello World");
     }
 
     //todo Task 2
     public void helloRobot(){
-        // input your solution here
+        System.out.println("0123456789012345678901    ");
+        System.out.println("         __               ");
+        System.out.println(" _(\\    |@@|             ");
+        System.out.println("(__/\\__ \\--/ __         ");
+        System.out.println("   \\___|----|  |   __    ");
+        System.out.println("       \\ }{ /\\ )_ / _\\ ");
+        System.out.println("       /\\__/\\ \\__O (__ ");
+        System.out.println("      (--/\\--)    \\__/  ");
+        System.out.println("      _)(  )(_            ");
+        System.out.println("     `---''---`           ");
     }
 
     //todo Task 3
